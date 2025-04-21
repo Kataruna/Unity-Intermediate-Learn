ef2994864e62973ff89fc0b6a27361db2b3bb775
Kataruna <tuaa1bb2@gmail.com>
2025-04-21 18:22:01 +0700

    Interface

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -54,7 +54,9 @@ public class PlayerController : MonoBehaviour
     // TODO: Make it easier to read
     void FixedUpdate()
     {
+        _speed = _isRunning ? _runSpeed : _walkSpeed;
         Vector3 movement = new Vector3(_movementInput.x, 0, _movementInput.y);
+        
         movement *= _speed * 0.1f;
         transform.Translate(movement, Space.Self);
 
@@ -74,4 +76,14 @@ public class PlayerController : MonoBehaviour
             _animator.transform.forward = direction;
         }
     }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent<ICollectable>(out ICollectable item))
+        {
+            item.Collect();
+        }
+    }
+
 }
+  
